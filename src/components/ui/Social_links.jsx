3f08867646f5c3e944a5453f8dd2c377ebc9d1f9
1HusniddinUz <husniddinhalimov112@gmail.com>
2025-10-04 15:13:12 +0500

    vercel analytics added

--- src/components/ui/Social_links.jsx
+++ src/components/ui/Social_links.jsx
@@ -4,14 +4,14 @@ const SocialLinks = () => {
     return (
         <div id="SocialLinks">
             <div className="container">
-                <a href="https://instagram.com" target="_blank" rel="noreferrer">
+                <a href="https://instagram.com/sabah_fashion_house" target="_blank" rel="noreferrer">
                     <button>Instagram</button>
                 </a>
                 <a href="https://t.me/Sabah_fashion_house" target="_blank" rel="noreferrer">
                     <button>Telegram</button>
                 </a>
-                <a href="https://facebook.com" target="_blank" rel="noreferrer">
-                    <button>Facebook</button>
+                <a  href="https://mail.google.com/mail/?view=cm&fs=1&to=sayitovabarchinoy87@gmail.com&body=Assalomu%20alaykum" target="_blank" rel="noopener noreferrer">
+                    <button>EMAIL</button>
                 </a>
             </div>
         </div>
